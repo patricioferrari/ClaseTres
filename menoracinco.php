6be5a03a859534c07da508cb4378638cf8ace0c3
patricioferrari <ferrari.patricio@yahoo.com.ar>
2016-08-30 21:24:56 -0300

    Aplicamos estilos y animaciones .css cuando mostramos mayor o menor a 5

--- menoracinco.php
+++ menoracinco.php
@@ -1,3 +1,20 @@
-<?php
-	echo "El mensaje ".$_POST['mensaje']." es menor a 5";
-?>
+<html>
+<head>
+	<meta charset="utf-8">
+		<title>Curso Ingreso UTN FRA</title>
+
+		
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<link rel="stylesheet" type="text/css" href="animacion.css">
+		
+</head>
+<body>
+	<div class="CajaInicio">
+	<h1>
+	<?php
+		
+		echo "El mensaje ".$_POST['mensaje']." es menor a 5";
+	?>
+	</h1>
+</body>
+</html>
